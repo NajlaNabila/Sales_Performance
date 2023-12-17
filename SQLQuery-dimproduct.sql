0bc2a0c474069b3846f45c0942ca8e87671d1988
Nur Najla Nabila bt Azman <154044136+NajlaNabila@users.noreply.github.com>
2023-12-17 15:22:19 +0800

    Add files via upload

---
+++ SQLQuery-dimproduct.sql
@@ -0,0 +1,49 @@
+-- Cleansed DIM_Product Table --
+SELECT 
+  p.[ProductKey], 
+  p.[ProductAlternateKey] AS ProductItemCode, 
+  --,[ProductSubcategoryKey]
+  --,[WeightUnitMeasureCode]
+  --,[SizeUnitMeasureCode]
+  p.[EnglishProductName] AS [Product Name], 
+  ps.EnglishProductSubcategoryName AS [Sub Category], --Joined in fro Sub Category Table
+  pc.EnglishProductCategoryName AS [Product Category], -- Joined in from Category Table
+  --,[SpanishProductName]
+  --,[FrenchProductName]
+  --,[StandardCost]
+  --,[FinishedGoodsFlag]
+  p.[Color] AS [Product Colour], 
+  --,[SafetyStockLevel]
+  --,[ReorderPoint]
+  --,[ListPrice]
+  p.[Size] AS [Product Size], 
+  --,[SizeRange]
+  --,[Weight]
+  --,[DaysToManufacture]
+  p.[ProductLine] AS [Product Line], 
+  --,[DealerPrice]
+  --,[Class]
+  --,[Style]
+  p.[ModelName] AS [Product Model Name], 
+  --,[LargePhoto]
+  p.[EnglishDescription] AS [Product Description], 
+  --,[FrenchDescription]
+  --,[ChineseDescription]
+  --,[ArabicDescription]
+  --,[HebrewDescription]
+  --,[ThaiDescription]
+  --,[GermanDescription]
+  --,[JapaneseDescription]
+  --,[TurkishDescription]
+  --,[StartDate]
+  --,[EndDate]
+  ISNULL (p.Status, 'Outdated') AS [Product Status] -- replaced null row with 'outdated'
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimProduct] AS p 
+  LEFT JOIN dbo.DimProductSubcategory AS ps ON ps.ProductSubcategoryKey = p.ProductSubcategoryKey 
+  LEFT JOIN dbo.DimProductCategory AS pc ON pc.ProductCategoryKey = ps.ProductCategoryKey 
+ORDER BY 
+  p.ProductKey ASC
+
+
+
